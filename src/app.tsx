b5b1f94f8312acf68f60bbda5f121b900ab5fe2a
Erick Gustavo <erick.stazs@gmail.com>
2022-09-05 20:03:36 -0300

    Add useState example

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,23 @@
+import { useState } from "react"
+
 function App() {
-  return <h1>App</h1>
+  const [counter, setCounter] = useState(0)
+  const handlePlus = () => setCounter((counter) => counter + 1)
+  const handleMinus = () => setCounter((counter) => counter - 1)
+  const plusThree = () => {
+    setCounter(counter => counter + 1)
+    setCounter(counter => counter + 1)
+    setCounter(counter => counter + 1)
+  }
+
+  return (
+    <>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleMinus}>-</button>
+      <button type="button" onClick={handlePlus}>+</button>
+      <button type="button" onClick={plusThree}>+++</button>
+    </>
+  )
 }
 
 export { App }
