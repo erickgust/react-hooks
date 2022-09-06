29c35f884d6b9e6bf6a43aab8cdb780ca59fb1b7
Erick Gustavo <erick.stazs@gmail.com>
2022-09-05 21:53:30 -0300

    Add useEffect example

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
   const [counter, setCounter] = useState(0)
+  const [name, setName] = useState('')
+
   const handlePlus = () => setCounter((counter) => counter + 1)
   const handleMinus = () => setCounter((counter) => counter - 1)
   const plusThree = () => {
@@ -10,12 +12,32 @@ function App() {
     setCounter(counter => counter + 1)
   }
 
+  useEffect(() => {
+    console.log('first render')
+
+    return () => console.log('unmount')
+  }, [])
+
+  useEffect(() => {
+    console.log('counter')
+  }, [counter])
+
+  useEffect(() => {
+    console.log('name')
+  }, [name])
+
   return (
     <>
       <h1>{counter}</h1>
       <button type="button" onClick={handleMinus}>-</button>
       <button type="button" onClick={handlePlus}>+</button>
       <button type="button" onClick={plusThree}>+++</button>
+
+      <div>
+        <span>{name}</span>
+        <br />
+        <input onChange={e => setName(e.target.value)} />
+      </div>
     </>
   )
 }
