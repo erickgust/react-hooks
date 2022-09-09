07e0a0dd09ac12561d25106f0d67568e7c563cfd
Erick Gustavo <erick.stazs@gmail.com>
2022-09-08 23:50:22 -0300

    Add useMemo example

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,30 @@
-import { useCallback, useState } from "react"
-
-const fnCounter = new Set()
+import { useCallback, useMemo, useState } from "react"
 
 function App() {
   const [counter, setCounter] = useState(0)
+  const [name, setName] = useState('')
+
+  const total = useMemo(() => {
+    console.log('useMemo')
+    return counter * 12323213 * 3231313 * 123233
+  }, [counter])
 
   const handlePlus = useCallback(() => {
     setCounter((prevState) => prevState + 1)
   }, [])
 
-  fnCounter.add(handlePlus)
-  console.log(fnCounter.size)
-
   return (
     <div>
       <h1>{counter}</h1>
-      <Button onClick={handlePlus}/>
+      <h4>{total}</h4>
+      <button type="button" onClick={handlePlus}>+</button>
+
+      <br />
+      <span>{name}</span>
+      <br />
+      <input type="text" onChange={e => setName(e.target.value)} />
     </div>
   )
 }
-
-type ButtonProps = {
-  onClick: () => void
-}
-
-const Button = ({ onClick }: ButtonProps) => (
-  <button type="button" onClick={onClick}>+</button>
-)
 
 export {App}
