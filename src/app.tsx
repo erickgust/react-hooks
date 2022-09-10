ee60fc1a72e385372a22242c8a07e1f8d4db4e2d
Erick Gustavo <erick.stazs@gmail.com>
2022-09-09 23:58:19 -0300

    Add useRef example

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,19 @@
-import { useCallback, useMemo, useState } from "react"
+import { useRef } from "react"
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  const [name, setName] = useState('')
+  console.log('App render')
 
-  const total = useMemo(() => {
-    console.log('useMemo')
-    return counter * 12323213 * 3231313 * 123233
-  }, [counter])
+  const inputRef = useRef<HTMLInputElement | null>(null)
 
-  const handlePlus = useCallback(() => {
-    setCounter((prevState) => prevState + 1)
-  }, [])
+  function handlePrintName() {
+    if (!inputRef.current) return
+    alert(inputRef.current.value)
+  }
 
   return (
     <div>
-      <h1>{counter}</h1>
-      <h4>{total}</h4>
-      <button type="button" onClick={handlePlus}>+</button>
-
-      <br />
-      <span>{name}</span>
-      <br />
-      <input type="text" onChange={e => setName(e.target.value)} />
+      <input type="text" ref={inputRef} />
+      <button type="button" onClick={handlePrintName}>Print name</button>
     </div>
   )
 }
